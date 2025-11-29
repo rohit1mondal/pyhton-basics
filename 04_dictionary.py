f10211aa1f377fcd83f7de29ebb9e518e29400e4
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-11-29 12:45:19 +0530

    methods of dictionaries

--- 04_dictionary.py
+++ 04_dictionary.py
@@ -11,3 +11,26 @@ print(info)
 null_dict = {}
 null_dict["name"] = "john"
 print(null_dict)
+
+# nested dictionary
+
+student = {
+    "name": "Alice",
+    "subjects": {
+        "math":97,
+        "physics": 70,
+        "chemistry": 64
+    }
+}
+print(student["subjects"]["math"])
+
+# methods
+print(info.keys())
+print(info.values())
+print(info.items())
+print(student["name"])
+print(student.get("name"))
+
+new_info = {"city":"Kolkata"}
+student.update(new_info)
+print(student)
