deb4393ab47a74b46976121e59889ef5b90b7a94
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-11-29 12:20:37 +0530

    dictionaries and sets

---
+++ 04_dictionary.py
@@ -0,0 +1,13 @@
+info = {
+    "name" : "apnacollege",
+    "subjects": ["python", "C", "java"],
+    "topics": ("dictionary", "set"),
+    "age": 24,
+    "is_adult": True,
+    45: 456.23
+}
+print(info)
+
+null_dict = {}
+null_dict["name"] = "john"
+print(null_dict)
